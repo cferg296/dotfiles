38c9b441e5e40a3938efa53d4b0c7c29ce23f7d9
cferg296 <cpdf98@yahoo.com>
2026-08-23 10:11:32 -0400

    Removed xwayland video bridge
    Out of date AUR package

--- hypr/hyprland/general/rules.lua
+++ hypr/hyprland/general/rules.lua
@@ -71,17 +71,3 @@ hl.window_rule({
     center = true;
     size = { 1750, 900 }
 })
-
--- XWayland Video Bridge
-hl.window_rule({
-    name = "xwayland-video-bridge-fixes",
-    match = {
-        class = "xwaylandvideobridge",
-    },
-    no_initial_focus = true,
-    no_focus = true,
-    no_anim = true,
-    no_blur = true,
-    max_size = { 1, 1 },
-    opacity = "0.0",
-})
